996bd80db6f6883c2eea15deab45e67d78817a9a
harry-mca <ham5649@psu.edu>
2026-04-22 09:40:06 -0400

    Added CSV files to directory
    Downloaded and added relevant cvs files to the directory; created new project to house wrangling file for future use.

---
+++ NFLStatistics/NFLStatisticsWrangling.R
@@ -0,0 +1,11 @@
+
+library(tidyverse)
+library(readr)
+
+gameOutcomesRaw <- read.csv("nfl_mahomes_era_games.csv")
+teamStatisticsRaw <- read.csv("nfl-team-statistics.csv")
+
+View(gameOutcomesRaw)
+View(teamStatisticsRaw)
+
+
